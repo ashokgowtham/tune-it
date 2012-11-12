8243ef9b71a9faaf06da3a8f54545656167f70ac
ashokgowtham <ashokgowtham123@gmail.com>
2012-11-12 23:36:33 +0530

    Ashok | file_additions | Added Tone and NoteRenderer

---
+++ src/TuneIt/Engine/NoteRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+using TuneIt.Model;
+
+namespace TuneIt.Engine
+{
+    public class NoteRenderer
+    {
+        private readonly Note note;
+        private readonly Tone tone;
+
+        public NoteRenderer(Note note, Tone tone)
+        {
+            this.note = note;
+            this.tone = tone;
+        }
+
+        public void Render(Stream stream)
+        {
+        }
+    }
+}
---
+++ src/TuneIt/Model/Tone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TuneIt.Model
+{
+    /// <summary>
+    /// Tone : time => audioFrame
+    /// </summary>
+    public class Tone
+    {
+        private readonly string expression;
+        private readonly Func<double, double> lambda;
+        private readonly ToneSource source;
+
+        public Tone(string toneExpression)
+        {
+            expression = toneExpression;
+            source = ToneSource.FromExpression;
+        }
+
+        public Tone(Func<double, double> lambdaExpression)
+        {
+            lambda = lambdaExpression;
+            source = ToneSource.FromLambda;
+        }
+    }
+
+    public enum ToneSource
+    {
+        FromExpression,
+        FromLambda
+    }
+}
